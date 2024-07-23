a1d18b420c525798aa5e2a5f4f8fe6a75987779d
Anurag Gaddamwar <123613177+Anurag-Gaddamwar@users.noreply.github.com>
2024-07-23 18:49:20 +0530

    Update resume.jsx

--- pages/resume.jsx
+++ pages/resume.jsx
@@ -157,7 +157,7 @@ const Resume = () => {
             id="jobRole"
             value={jobRole}
             onChange={(e) => setJobRole(e.target.value)}
-            placeholder="Enter the job role you are aiming for; Ex: Graphic Designer"
+            placeholder="Ex: Graphic Designer"
             className="w-full px-4 py-2 text-black rounded-md border border-gray-300 focus:outline-none focus:border-blue-500"
           />
         </div>
