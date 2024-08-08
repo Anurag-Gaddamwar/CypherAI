b6ab11ce5f6c1117cbd06b287a17ab6f871e317e
Anurag Gaddamwar <123613177+Anurag-Gaddamwar@users.noreply.github.com>
2024-08-09 00:55:31 +0530

    Update Navbar.jsx

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -3,15 +3,18 @@ import Link from 'next/link';
 
 const Navbar = () => {
   return (
-    <nav className="bg-gradient-to-b z-20 fixed top-0 left-0 right-0 from-gray-800 font-sans text-[1.07rem] text-white p-5">
+    <nav className="bg-gradient-to-b z-20 fixed top-0 left-0 right-0 from-gray-800 font-sans text-[1.07rem] text-white p-5 backdrop-blur-md backdrop-filter">
       <div className="mx-auto">
         <div className="flex justify-between items-center">
           <div className="flex space-x-4">
             <Link href="/">
-              <p className=" hover:text-blue-300 transition-colors duration-300">AI</p>
+              <p className="hover:text-blue-300 transition-colors duration-300">AI</p>
             </Link>
             <Link href="/resume">
               <p className="text-white hover:text-blue-300 transition-colors duration-300">Resume</p>
+            </Link>
+            <Link href="/interview">
+              <p className="text-white hover:text-blue-300 transition-colors duration-300">Interview</p>
             </Link>
           </div>
         </div>
