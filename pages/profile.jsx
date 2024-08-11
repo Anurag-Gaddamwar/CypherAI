fdf69f2ddf3489e820c2467bebf1b9d028458bac
Anurag Gaddamwar <123613177+Anurag-Gaddamwar@users.noreply.github.com>
2024-08-11 11:23:19 +0530

    Create profile.jsx

---
+++ pages/profile.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../src/app/globals.css';
+import Navbar from '../src/app/components/Navbar';
+
+const profile = () => {
+  return (
+    <div className="max-h-screen font-sans text-sm bg-black text-white flex flex-col">
+        <Navbar />
+        
+    </div>
+  )
+}
+
+export default profile;
