53d082e8e6db8856204a1fcac6ec776beacf35c3
Anurag Gaddamwar <123613177+Anurag-Gaddamwar@users.noreply.github.com>
2024-10-28 00:51:49 +0530

    Update profile.jsx

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import '../src/app/globals.css';
 import Navbar from '../src/app/components/Navbar';
-
+import Fragment from 'react';
 const profile = () => {
   return (
     <Fragment>
