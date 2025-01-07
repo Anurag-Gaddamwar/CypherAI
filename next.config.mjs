a7de47b6762885ba6ae4b17ea02c2e6e5a4b5e19
Anurag Gaddamwar <123613177+Anurag-Gaddamwar@users.noreply.github.com>
2025-01-07 17:03:05 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    webpack(config, { isServer }) {
+      if (!isServer) {
+        // Mock 'fs' for the client-side
+        config.resolve.fallback = {
+          fs: false,
+        };
+      }
+      return config;
+    },
+  };
+  
+  export default nextConfig;
+  
